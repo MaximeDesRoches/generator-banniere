794ad716bec2e54f28a0a73b2c4afc42ee9e6d5a
Maxime Des Roches <m.desroches@la-grange.ca>
2016-06-02 11:59:36 -0400

    initial commit

---
+++ app/index.js
@@ -0,0 +1,82 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+
+
+var getSalt = function (len) {
+	len = len || 20;
+	var set = '0123456789ABCDEFGHIJKLMNOPQURSTUVWXYZ -!$%?&*()=_+|£¢@{}[];:',
+		setLen = set.length,
+		salt = '';
+	for (var i = 0; i < len; i++) {
+		var p = Math.floor(Math.random() * setLen);
+		salt += set[p];
+	}
+	return salt;
+}
+
+var LagrangeGenerator = yeoman.generators.Base.extend({
+	init: function () {
+		this.pkg = require('../package.json');
+
+		this.on('end', function () {
+			if (!this.options['skip-install']) {
+				this.installDependencies({
+					callback: function () {
+						this.spawnCommand('git', ['archive','--remote=https://bitbucket.org/lg2fabrique/html5_banner_template','HEAD:dist','lib.min.js','|','tar','-x']);
+					}.bind(this)
+				});
+			}
+		});
+	},
+
+	askFor: function () {
+		var done = this.async();
+
+		// have Yeoman greet the user
+		// this.log(this.yeoman);
+		this.log(chalk.yellow(
+		'          _ _,---._ \n       ,-\',\'       `-.___ \n      /-;\'               `._ \n     /\\/          ._   _,\'o \\ \n    ( /\\       _,--\'\\,\',\'"`. ) \n     |\\      ,\'o     \\\'    //\\ \n     |      \\        /   ,--\'""`-. \n     :       \\_    _/ ,-\'         `-._ \n      \\        `--\'  /                ) \n       `.  \\`._    ,\'     ________,\',\' \n         .--`     ,\'  ,--` __\\___,;\' \n          \\`.,-- ,\' ,`_)--\'  /`.,\' \n           \\( ;  | | )      (`-/ \n             `--\'| |)       |-/ \n               | | |        | | \n               | | |,.,-.   | |_ \n               | `./ /   )---`  ) \n              _|  /    ,\',   ,-\' \n     -hrr-   ,\'|_(    /-<._,\' |--, \n             |    `--\'---.     \\/ \\ \n             |          / \\    /\\  \\ \n           ,-^---._     |  \\  /  \\  \\ \n        ,-\'        \\----\'   \\/    \\--`. \n       /            \\              \\   \\ '
+		));
+
+		// replace it with a short and sweet description of your generator
+		this.log(chalk.red('On a eu un nouveau projet sur c\'tes affaire-là d\'internet. Savez-vous ce qu\'on va faire avec c\'te 400 piasses-là?'));
+
+		//console.log(this);
+
+		var getDefaultFromPrevious = function(propName) {
+			return function(answers){
+				return answers[propName];
+			}
+		};
+
+		var prompts = [
+			{
+				name: 'projectName',
+				type: 'input',
+				message: 'Nom du client/projet',
+				default : this.appname,
+			}
+		];
+
+		this.prompt(prompts, function (answers) {
+			this.props = answers;
+			function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+			this.props.projectNamespace = this._.camelize(this.props.projectName);
+			done();
+		}.bind(this));
+	},
+
+	app: function () {
+		this.template('_package.json', 'package.json');
+		this.template('_gulpfile.js', 'gulpfile.js');
+		this.template('_.gitignore', '.gitignore');
+		this.directory('default');
+		this.mkdir('html');
+	},
+});
+
+module.exports = LagrangeGenerator;
---
+++ app/templates/_gulpfile.js
@@ -0,0 +1,99 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var bundleFolder = 'bundled';
+var packageFolder = '';
+var fs = require("fs");
+var bannersFolder = 'html';
+var formatRegex = /^(en|fr)?_?([\d]+x[\d]+)_?(.*)/i;
+var zip = require('gulp-zip');
+var spritesmith = require('gulp.spritesmith');
+
+function generateSpritesheets(file) {
+	var dest = file.path.replace(/[\/\\][\w_\-\.]+\.\w+$/, '');
+	//console.log(file.path);
+	//console.log(dest);
+    var spriteData = gulp.src(dest + '/*')
+        .pipe(spritesmith({
+            imgName: 'spritesheet.png',
+            cssName: 'sprites.scss'
+        }));
+
+    spriteData.img.pipe(gulp.dest(dest + '/../'));
+    spriteData.css.pipe(gulp.dest(dest + '/../').on('end', function(){
+		console.log('Spritesheet generated');
+	}));
+};
+
+
+
+//***************************************************
+
+function compileScss(file){
+	var dest = file.path.replace(/[\/\\][a-z_\-\.]+\.scss$/, '');
+	//console.log(dest)
+
+	return gulp.src(file.path)
+		// .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(dest).on('end',function(){
+			console.log('Sass compiled.');
+		}));
+}
+
+
+function bundleZip(){
+
+	try {
+		fs.statSync(bundleFolder);
+	} catch(e) {
+		fs.mkdirSync(bundleFolder);
+	}
+
+	var zips = null;
+	var bannersGroup = fs.readdirSync(bannersFolder).filter(function(f){
+		return f != ".DS_Store";
+	}).forEach(function(dirname){
+		var banners = fs.readdirSync(bannersFolder + '/' + dirname).map(function(fname) {
+			var m = fname.match(formatRegex);
+			if(m) {
+				return {
+					lng: m[1],
+					format: m[2],
+					name: m[3],
+					full: m[0],
+				}
+			}
+			return false;
+		}).filter(function(f){
+			return f;
+		}).forEach(function(cnf){
+			// console.log(cnf.full);
+
+			console.log("Zipping:", dirname+'/'+cnf.full);
+
+			var base = bannersFolder + '/' + dirname + '/' + cnf.full;
+			var stream = gulp.src([base + '/**', '!'+base+'/spritesheet_src/**', '!'+base+'/spritesheet_src/', '!'+base+'/**/*.scss'])
+							.pipe(zip(cnf.full+'.zip'))
+							.pipe(gulp.dest(bundleFolder+'/'+dirname));
+		});
+	});
+
+	return zips;
+}
+
+//************** TASKS
+
+gulp.task('scss', compileScss);
+
+gulp.task('watch', function () {
+
+	gulp.watch('**/**/styles.scss').on('change', compileScss);
+	gulp.watch('**/**/spritesheet_src/*.*').on('change', generateSpritesheets);
+
+});
+
+gulp.task('bundle', function() {
+	return bundleZip();
+});/**/

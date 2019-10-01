d8cc252382bde79735175b1ae252fe558aeec287
Maxime Des Roches <m.desroches@la-grange.ca>
2019-10-01 13:51:13 -0400

    QA

--- generators/templates/deploy.sh
+++ generators/templates/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-  rsync -avzL --progress --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r ./minified/. ubuntu@enclos.ca:/var/www/clients.enclos.ca/public_html/richmedia/<%= projectName %>
+gulp enclos
+rsync -avzL --progress --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r ./minified/. ubuntu@enclos.ca:/var/www/clients.enclos.ca/public_html/richmedia/<%= projectName %>
